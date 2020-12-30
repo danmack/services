a4c15cc4904e44c7f74eeaae6078595ce2969414
Marshall Onsrud <monsrud@kwikbit.com>
2020-12-30 11:05:21 -0600

    updates

--- samba/README.md
+++ samba/README.md
@@ -2,14 +2,10 @@
 
 This container is a samba server with wide open permissions on the share.
 
-Edit build.sh to specify the share path on the local host where files will be saved. Also,
-specify the name of the container. The default container name is samba-server-eee48.
-Each time the build.sh is run, the container will be stopped and removed.
+Ensure you don't have a samba server already running on the parent host.
 
-Run build.sh in this directory to start the server.
+Run: sh ./build.sh in this directory to build the server.
 
-Here is an example of the run command:
-docker run --name samba-server -d -v /share:/share -p 137:137/udp -p 138:138/udp -p 139:139 -p 445:445 0e94e8243668
 
 
 
--- samba/build.sh
+++ samba/build.sh
@@ -1,25 +1,53 @@
 #!/bin/sh
 
-CONTAINER_NAME="samba-server-eee48"
+CONTAINER_NAME="samba-server-lab"
 
-# the name of the directory on the host where the smb files will be stored
-# if you change the name here, you also need to change it in the smb.conf.
-LOCAL_SHARE="/share"
+LOCAL_SHARE="share"
 
 docker container stop ${CONTAINER_NAME}  > /dev/null 2>&1
 docker container rm ${CONTAINER_NAME}  > /dev/null 2>&1
 
+# Simple samba config
+
+cat << EOF > smb.conf 
+
+#
+# Example smb.conf for a wide open share
+#
+[global]
+   workgroup = WORKGROUP
+   security =  user
+   map to guest = bad user
+   guest account = nobody
+
+[share]
+    comment = share 
+    public = yes
+    path = share
+    read only = no
+    guest ok = yes
+    only guest = yes
+    writable = yes
+    force group = nogroup
+    force user = nobody
+    force create mode = 0755
+    force directory mode = 0755
+    browsable = yes
+
+EOF
+
 mkdir -p ${LOCAL_SHARE} 
 
 # Group may vary between nogroup and nobody
-chown -R nobody:nogroup /share
-chmod -R ugo+rwx /share
+chown -R nobody:nogroup ${LOCAL_SHARE} 
+chmod -R ugo+rwx ${LOCAL_SHARE}
 
 OUTPUT=$(docker build --rm=true --force-rm=true . | grep "Successfully built")
-echo $OUTPUT
 CONTAINER=$(echo $OUTPUT | cut -d' ' -f3)
+echo $OUTPUT
 echo ""
 echo "Start your container with the command:"
-echo "docker run --name ${CONTAINER_NAME} -d -v ${LOCAL_SHARE}:/share -p 137:137/udp -p 138:138/udp -p 139:139 -p 445:445 ${CONTAINER}"
+echo ""
+echo "docker run --name ${CONTAINER_NAME} -d -v ${PWD}/${LOCAL_SHARE}:/share -p 137:137/udp -p 138:138/udp -p 139:139 -p 445:445 ${CONTAINER}"
 
-
+rm smb.conf
